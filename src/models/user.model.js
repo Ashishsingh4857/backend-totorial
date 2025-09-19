c7d74ffcef1323ff05896a169af9e30f67e7e60f
Ashish singh <ashishsingh4857@gmail.com>
2025-09-20 02:27:15 +0530

    feat:implement User model with schema,password hashing with bcrypt  and jwt token generation

---
+++ src/models/user.model.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // Only hash if password is modified or new
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Check if password is correct
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generate JWT AccessToken
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this.ObjectId,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    process.env.ACCESS_TOKEN_EXPIRY
+  );
+};
+// Generate JWT AccessToken
+
+export const User = mongoose.model("User", userSchema);
