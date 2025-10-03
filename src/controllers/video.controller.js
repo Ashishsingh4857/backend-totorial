985caa680fc173cd51915d46eeb88290765311de
Ashish singh <ashishsingh4857@gmail.com>
2025-10-03 20:54:34 +0530

    feat: improve error handling in video publishing and retrieval functions add delete video logic

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -13,34 +13,27 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description, duration } = req.body;
-  if (!title) {
-    new ApiError(400, "title is required");
-  }
-  if (!description) {
-    new ApiError(400, "description is required");
-  }
-  if (!duration) {
-    new ApiError(400, "duration is required");
-  }
+  if (!title) throw new ApiError(400, "title is required");
+
+  if (!description) throw new ApiError(400, "description is required");
+
+  if (!duration) throw new ApiError(400, "duration is required");
+
   // get video, upload to cloudinary, create video
   const videoFileLocalPath = req.files?.videoFile?.[0]?.path;
   const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path;
 
-  if (!videoFileLocalPath) {
-    new ApiError(400, "video file  is required");
-  }
-  if (!thumbnailLocalPath) {
-    new ApiError(400, "thumbnail is required");
-  }
+  if (!videoFileLocalPath) throw new ApiError(400, "video file  is required");
+
+  if (!thumbnailLocalPath) throw new ApiError(400, "thumbnail is required");
+
   // upload local files in Cloudinary
   const video = await uploadOnCloudinary(videoFileLocalPath);
   const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
-  if (!video?.url) {
-    new ApiError(500, "something went wrong");
-  }
-  if (!thumbnail.url) {
-    new ApiError(500, "something went wrong");
-  }
+  if (!video?.url) throw new ApiError(500, "something went wrong");
+
+  if (!thumbnail.url) throw new ApiError(500, "something went wrong");
+
   //upload in database
   const createdVideo = await Video.create({
     title,
@@ -52,9 +45,9 @@ const publishAVideo = asyncHandler(async (req, res) => {
   //check video is created
   const publishedVideo = await Video.findById(createdVideo._id);
 
-  if (!publishedVideo) {
-    new ApiError(500, "something went wrong while uploading video");
-  }
+  if (!publishedVideo)
+    throw new ApiError(500, "something went wrong while uploading video");
+
   return res
     .status(201)
     .json(new ApiResponse(200, publishedVideo, "video published successfully"));
@@ -62,41 +55,35 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  if (!videoId) {
-    new ApiError(400, "VIDEO ID is required ");
-  }
+  if (!videoId) throw new ApiError(400, "VIDEO ID is required ");
+
   //find in db
-  const video = await Video.findById(videoId);
-  if (!video) {
-    new ApiError(404, "video not found");
-  }
+  const videoExist = await Video.findById(videoId);
+  if (!videoExist) throw new ApiError(404, "video not found");
+
   return res
     .status(200)
-    .json(new ApiResponse(200, video, "video fetch successfully"));
+    .json(new ApiResponse(200, videoExist, "video fetch successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
-  if (!videoId) {
-    new ApiError(400, "VIDEO ID is required ");
-  }
+  if (!videoId) throw new ApiError(400, "VIDEO ID is required ");
+
   const thumbnailLocalPath = req.file?.path;
-  if (!thumbnailLocalPath) {
-    new ApiError(400, "file is required");
-  }
+  if (!thumbnailLocalPath) throw new ApiError(400, "file is required");
+
   //update on Cloudinary
   const updatedThumbnail = await uploadOnCloudinary(thumbnailLocalPath);
-  if (!updatedThumbnail?.url) {
-    new ApiError(500, "something went wrong");
-  }
+  if (!updatedThumbnail?.url) throw new ApiError(500, "something went wrong");
+
   // todo : delete old file from Cloudinary
   const video = await Video.findByIdAndUpdate(videoId, {
     $set: { thumbnail: updatedThumbnail.url },
   });
-  if (!video) {
-    new ApiError(500, "something went wrong while updating video");
-  }
+  if (!video)
+    throw new ApiError(500, "something went wrong while updating video");
 
   return res
     .status(200)
@@ -105,7 +92,15 @@ const updateVideo = asyncHandler(async (req, res) => {
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: delete video
+  if (!videoId) throw new ApiError(400, "VIDEO ID is required ");
+
+  const videoExist = await Video.findById(videoId);
+
+  if (!videoExist) throw new ApiError(400, "video does not exist");
+  await Video.deleteOne({ _id: videoExist?._id });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
