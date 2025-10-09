1852543f46df09e6e438f4ca1f4d49036fcd822e
Ashish singh <ashishsingh4857@gmail.com>
2025-10-09 22:52:13 +0530

    feat: add like routes for managing video ,tweet and comment likes

--- src/app.js
+++ src/app.js
@@ -31,12 +31,14 @@ import healthCheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter); // path or handler
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
 
 //example url -  http://localhost:3000/api/v1/users/register
 export { app, PORT };
