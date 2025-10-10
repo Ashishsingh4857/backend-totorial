958ef8ff0e85dc5e56566860c667981afdc6f343
Ashish singh <ashishsingh4857@gmail.com>
2025-10-11 01:01:07 +0530

    feat: implement create ,update and delete tweet functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,32 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Tweet } from "../models/tweet.model.js";
+import { isValidObjectId } from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const createTweet = asyncHandler(async (req, res) => {
-  // TODO: create tweet
+  const { content } = req.body;
+
+  // Input validation
+  if (!content || typeof content !== "string" || content.trim() === "") {
+    throw new ApiError(400, "Content field is required");
+  }
+  //create tweet in db
+  try {
+    const newTweet = await Tweet.create({
+      content: content.trim(),
+      owner: req.user?._id,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, newTweet, "Tweet created successfully"));
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Internal server error. Unable to create tweet",
+      error
+    );
+  }
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
@@ -8,11 +34,79 @@ const getUserTweets = asyncHandler(async (req, res) => {
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
-  //TODO: update tweet
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  // Input validation
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweetId");
+  }
+
+  if (!content || typeof content !== "string" || content.trim() === "") {
+    throw new ApiError(400, "Content field is required");
+  }
+  //existence check ownership check
+  const existingTweet = await Tweet.findOne({
+    _id: tweetId,
+    owner: req.user._id,
+  });
+
+  if (!existingTweet) {
+    throw new ApiError(404, "Tweet not found or you don't have permission");
+  }
+
+  //update in db
+  try {
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+      tweetId,
+      {
+        $set: {
+          content: content.trim(),
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Internal server error. Unable to update tweet",
+      error
+    );
+  }
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
-  //TODO: delete tweet
+  const { tweetId } = req.params;
+  // Input validation
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweetId");
+  }
+  //existence check ownership check
+  const existingTweet = await Tweet.findOne({
+    _id: tweetId,
+    owner: req.user?._id,
+  });
+  if (!existingTweet) {
+    throw new ApiError(404, "Tweet not found or you don't have permission");
+  }
+  try {
+    await Tweet.deleteOne({ _id: tweetId });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "tweet deleted successfully"));
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Internal server error. Unable to delete tweet",
+      error
+    );
+  }
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
