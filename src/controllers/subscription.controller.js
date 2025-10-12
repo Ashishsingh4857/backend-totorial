c250e4ecb2a2f9bd4aff8b513e78e5ad112f6fc6
Ashish singh <ashishsingh4857@gmail.com>
2025-10-12 06:30:30 +0530

    feat: implement user channel subscriber and subscribed channels retrieval with input validation and aggregation

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Subscription } from "../models/subscription.model.js";
 import { User } from "../models/user.model.js";
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -69,10 +69,165 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  // Input validation
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channelId");
+  }
+
+  // Existence check
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(
+      400,
+      "Invalid channelId ! No channel found with this id"
+    );
+  }
+
+  const pipeline = [
+    {
+      $match: { channel: new mongoose.Types.ObjectId(channelId) },
+    },
+    // join with the users  collection
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
+    },
+    //This stage processes multiple pipelines within a single stage. In this case, it's used to calculate the subscribers and the subscribers count in a single pipeline
+    {
+      $facet: {
+        subscribers: [
+          {
+            $unwind: "$subscriber",
+          },
+          {
+            $project: {
+              _id: 0,
+              subscriber: "$subscriber",
+            },
+          },
+        ],
+        subscribersCount: [
+          {
+            $count: "count",
+          },
+        ],
+      },
+    },
+  ];
+
+  try {
+    const result = await Subscription.aggregate(pipeline);
+    console.log(result);
+
+    const subscribers = result[0].subscribers;
+    const subscribersCount = result[0].subscribersCount[0]?.count || 0;
+
+    if (!subscribers || subscribers.length === 0) {
+      throw new ApiError(404, "No subscribers found");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { subscribers, subscribersCount },
+          "Fetched User Channel Subscribers"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Internal server error. Unable to fetch User Channel Subscribers",
+      error
+    );
+  }
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  // Input validation
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriberId");
+  }
+
+  // Existence check
+  const user = await User.findById(subscriberId);
+  if (!user) {
+    throw new ApiError(
+      400,
+      "Invalid subscriberId ! No user found with this id"
+    );
+  }
+
+  const pipeline = [
+    {
+      $match: { subscriber: new mongoose.Types.ObjectId(subscriberId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "Channel",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $facet: {
+        Channels_Subscribed_To: [{ $unwind: "$Channel" }],
+        channels_Subscribed_To_Count: [{ $count: "count" }],
+      },
+    },
+  ];
+
+  try {
+    const result = await Subscription.aggregate(pipeline);
+    const subscribedChannels = result[0].Channels_Subscribed_To;
+    const subscribedChannelsCount =
+      result[0].channels_Subscribed_To_Count[0]?.count || 0;
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { subscribedChannels, subscribedChannelsCount },
+          "Fetched subscribed channels"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Internal server error. Unable to fetch subscribed channels",
+      error
+    );
+  }
 });
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
